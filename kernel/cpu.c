aad915566e36d65c065989ee503d3609588a151b
Lianwei Wang <lianwei.wang@gmail.com>
2016-06-09 23:43:28 -0700

    UPSTREAM: cpu/hotplug: Handle unbalanced hotplug enable/disable
    (cherry picked from commit 01b41159066531cc8d664362ff0cd89dd137bbfa)
    
    When cpu_hotplug_enable() is called unbalanced w/o a preceeding
    cpu_hotplug_disable() the code emits a warning, but happily decrements the
    disabled counter. This causes the next operations to malfunction.
    
    Prevent the decrement and just emit a warning.
    
    Signed-off-by: Lianwei Wang <lianwei.wang@gmail.com>
    Cc: peterz@infradead.org
    Cc: linux-pm@vger.kernel.org
    Cc: oleg@redhat.com
    Link: http://lkml.kernel.org/r/1465541008-12476-1-git-send-email-lianwei.wang@gmail.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ahmed Radaideh <ahmed.radaideh@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -206,7 +206,7 @@ static void __cpu_hotplug_enable(void)
 void cpu_hotplug_enable(void)
 {
 	cpu_maps_update_begin();
-	WARN_ON(--cpu_hotplug_disabled < 0);
+	__cpu_hotplug_enable();
 	cpu_maps_update_done();
 }
 
@@ -653,7 +653,7 @@ void __ref enable_nonboot_cpus(void)
 
 	/* Allow everyone to use the CPU hotplug again */
 	cpu_maps_update_begin();
-	WARN_ON(--cpu_hotplug_disabled < 0);
+	__cpu_hotplug_enable();
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
