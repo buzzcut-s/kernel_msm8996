f5feaffb4183ae37a373364f9ba73927b4650885
ahmedradaideh <ahmed.radaideh@gmail.com>
2018-07-19 00:36:53 +0300

    treewide: Remove some changes, This mismerge by CAF
    * This files For EAS scheduler only.
    
    Fixes: cc44956310afd676f4c74add235572cd8f52c871 ("Merge tag 'lsk-v3.18-16.11-android' into 'msm-3.18'")
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -514,9 +514,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
-extern struct cpufreq_governor cpufreq_gov_sched;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
 #endif
 
 /*********************************************************************
@@ -676,7 +673,4 @@ static inline void cpufreq_task_stats_remove_uids(uid_t uid_start,
 	uid_t uid_end) {}
 #endif
 
-struct sched_domain;
-unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
-unsigned long cpufreq_scale_max_freq_capacity(int cpu);
 #endif /* _LINUX_CPUFREQ_H */
